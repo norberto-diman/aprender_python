610087602df31b94ccb9b7199c402a533f69c5c0
Norberto Diman <norbertodimann@outlook.com.br>
2026-02-11 20:13:16 -0300

    changed the fibonacci.py file using recursive fuction

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,12 @@
-n = int(input("Que termo deseja encontrar: "))
-ultimo=1
-penultimo=1
+def fibonacci(n):
+    """Retorna o n-ésimo número da sequência de Fibonacci."""
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
-if (n==1) or (n==2):
-    print("1")
-else:
-    for count in range(2,n):
-        termo = ultimo + penultimo
-        penultimo = ultimo
-        ultimo = termo
-        count += 1
-    print(termo)
+# Primeiros 10 números de Fibonacci
+for i in range(30):
+    print(fibonacci(i), end=" ")  # Saída: 0 1 1 2 3 5 8 13 21 34
